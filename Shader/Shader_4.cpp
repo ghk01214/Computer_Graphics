9158e31a754990e4e64b476acaeb51822024fca2
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2020-10-06 02:35:40 +0900

    Update Shader_4.cpp

--- Shader/Shader_4.cpp
+++ Shader/Shader_4.cpp
@@ -1,17 +1,18 @@
+//실습 7번
 #include "Shader_4.h"
 
-void main(GLint iArgc, CHAR** cArgv)									//윈도우 출력하고 콜백함수 설정
+GLvoid main(GLint iArgc, CHAR** cArgv)									//윈도우 출력하고 콜백함수 설정
 {
 	//윈도우 생성하기
 	glutInit(&iArgc, cArgv);											// glut 초기화
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);					// 디스플레이 모드 설정
-	glutInitWindowPosition(0, 0);									// 윈도우의 위치 지정
-	glutInitWindowSize(800, 600);									// 윈도우의 크기 지정
-	glutCreateWindow("Excercise 1");								// 윈도우 생성 윈도우 이름
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);						// 디스플레이 모드 설정
+	glutInitWindowPosition(WINDOW_POS_X, WINDOW_POS_Y);					// 윈도우의 위치 지정
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);					// 윈도우의 크기 지정
+	glutCreateWindow("Shader 4");										// 윈도우 생성 윈도우 이름
 
 	//GLEW 초기화하기
 	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)										// glew 초기화
+	if (glewInit() != GLEW_OK)											// glew 초기화
 	{
 		std::cerr << "Unable to initialize GLEW" << std::endl;
 		exit(EXIT_FAILURE);
@@ -19,7 +20,7 @@ void main(GLint iArgc, CHAR** cArgv)									//윈도우 출력하고 콜백함수 설정
 	else
 		std::cout << "GLEW Initialized n";
 
-	glutDisplayFunc(DrawScene);										// 출력 콜백함수의 지정
-	glutReshapeFunc(Reshape);										// 다시 그리기 콜백함수 지정
-	glutMainLoop();													// 이벤트 처리 시작
+	InitializeBuffer();
+	InitializeShader();
+	FuctionalizeGlut();
 }
