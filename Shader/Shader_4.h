f902d1ada2e51ccb37863f128a4a426a3633d113
ghk01214 <ghk01214@naver.com>
2020-10-12 15:15:11 +0900

    Update Shader_4.h

--- Shader/Shader_4.h
+++ Shader/Shader_4.h
@@ -1,20 +1,10 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <random>
-#include <cmath>
-#include <gl/glew.h>
-#include <gl/freeglut.h>
-#include <gl/freeglut_ext.h>
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "Shader.h"
 #define WINDOW_POS_X 500
 #define WINDOW_POS_Y 500
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define VAO 1
-#define VBO 2
+#define VAO 10
+#define VBO 20
 
 typedef struct Position
 {
@@ -26,52 +16,6 @@ enum Direction
 {
 	Top, Right, Bottom, Left
 };
-
-class Shader
-{
-private:
-	GLuint uiVertexShader;
-	GLuint uiFragmentShader;
-	GLuint uiShaderProgramID;
-	GLuint uiShaderID;
-public:
-	Shader();
-	GLuint VertexShader();
-	GLuint FragmentShader();
-	GLuint ShaderProgram();
-	GLvoid InputShaderID(GLuint);
-	GLuint ShaderID();
-	~Shader();
-};
-
-Shader::Shader()
-{
-
-}
-GLuint Shader::VertexShader()
-{
-	return Shader::uiVertexShader;
-}
-GLuint Shader::FragmentShader()
-{
-	return Shader::uiFragmentShader;
-}
-GLuint Shader::ShaderProgram()
-{
-	return Shader::uiShaderProgramID;
-}
-GLvoid Shader::InputShaderID(GLuint ShaderID)
-{
-	Shader::uiShaderID = ShaderID;
-}
-GLuint Shader::ShaderID()
-{
-	return Shader::uiShaderID;
-}
-Shader::~Shader()
-{
-
-}
 
 class Object
 {
@@ -95,10 +39,15 @@ public:
 
 Object::Object()
 {
-	Object::pTriangleCenter.iX = 0.0f;
-	Object::pTriangleCenter.iY = -0.8f;
+	std::random_device rRandom;
+	std::mt19937 mGen(rRandom());
+	std::uniform_real_distribution<GLfloat> uRealDis(-1.0f, 1.0f);
+	std::uniform_int_distribution<GLint> uIntDis(Top, Left);
+
+	Object::pTriangleCenter.iX = uRealDis(mGen);
+	Object::pTriangleCenter.iY = uRealDis(mGen);
 	Object::pTriangleCenter.iZ = 1.0f;
-	Object::iMovingDirection = Direction::Top;
+	Object::iMovingDirection = uIntDis(mGen);
 
 	Object::ChangeColor();
 	Object::ChangePos();
@@ -115,8 +64,8 @@ GLvoid Object::ChangePos()
 	{
 	case Direction::Top:
 	{
-		Object::pTriangleCenter.iX -= 0.005f;
-		Object::pTriangleCenter.iY += 0.005f;
+		Object::pTriangleCenter.iX -= 0.01f;
+		Object::pTriangleCenter.iY += 0.01f;
 
 		for (GLint i = 0; i < 2; ++i)
 		{
@@ -131,8 +80,8 @@ GLvoid Object::ChangePos()
 	}
 	case Direction::Right:
 	{
-		Object::pTriangleCenter.iX += 0.005f;
-		Object::pTriangleCenter.iY += 0.005f;
+		Object::pTriangleCenter.iX += 0.01f;
+		Object::pTriangleCenter.iY += 0.01f;
 
 		for (GLint i = 0; i < 2; ++i)
 		{
@@ -147,8 +96,8 @@ GLvoid Object::ChangePos()
 	}
 	case Direction::Bottom:
 	{
-		Object::pTriangleCenter.iX += 0.005f;
-		Object::pTriangleCenter.iY -= 0.005f;
+		Object::pTriangleCenter.iX += 0.01f;
+		Object::pTriangleCenter.iY -= 0.01f;
 
 		for (GLint i = 0; i < 2; ++i)
 		{
@@ -163,8 +112,8 @@ GLvoid Object::ChangePos()
 	}
 	case Direction::Left:
 	{
-		Object::pTriangleCenter.iX -= 0.005f;
-		Object::pTriangleCenter.iY -= 0.005f;
+		Object::pTriangleCenter.iX -= 0.01f;
+		Object::pTriangleCenter.iY -= 0.01f;
 
 		for (GLint i = 0; i < 2; ++i)
 		{
@@ -184,6 +133,20 @@ GLvoid Object::ChangePos()
 GLfloat Object::Color(GLint i, GLint j)
 {
 	return Object::fColor[i][j];
+}
+GLvoid Object::ChangeColor()
+{
+	std::random_device rRandom;
+	std::mt19937 mGen(rRandom());
+	std::uniform_real_distribution<GLfloat> uRealDis(0.0f, 1.0f);
+
+	for (GLint i = 0; i < 3; ++i)
+	{
+		for (GLint j = 0; j < 3; ++j)
+		{
+			Object::fColor[i][j] = uRealDis(mGen);
+		}
+	}
 }
 GLvoid Object::ChangeDirection()
 {
@@ -207,28 +170,6 @@ GLvoid Object::ChangeDirection()
 		Object::iMovingDirection = Direction::Top;
 		Object::ChangeColor();
 	}
-}GLvoid Object::ChangeDirection()
-{
-	if (Object::fTrianglePos[0][0] < -1.0f)
-	{
-		Object::iMovingDirection = Direction::Right;
-		Object::ChangeColor();
-	}
-	else if (Object::fTrianglePos[0][1] > 0.8f)
-	{
-		Object::iMovingDirection = Direction::Bottom;
-		Object::ChangeColor();
-	}
-	else if (Object::fTrianglePos[0][0] > 1.0f)
-	{
-		Object::iMovingDirection = Direction::Left;
-		Object::ChangeColor();
-	}
-	else if (Object::fTrianglePos[0][1] < -0.8f)
-	{
-		Object::iMovingDirection = Direction::Top;
-		Object::ChangeColor();
-	}
 }
 BOOL Object::StartTimer()
 {
@@ -243,162 +184,51 @@ Object::~Object()
 
 }
 
-Shader mMaster;
-Object oOutside;
-GLuint uiVAO, uiVBO[VBO];
-
-GLchar* FileToBuf(const GLchar* cFile)
-{
-	FILE* fFile;
-	GLulong ulLength;
-	GLchar* cBuf;
-
-	fFile = fopen(cFile, "rb");
-
-	if (!fFile)
-		return NULL;
-
-	fseek(fFile, 0, SEEK_END);
-	ulLength = ftell(fFile);
-	cBuf = (GLchar*)malloc(ulLength + 1);
-	fseek(fFile, 0, SEEK_SET);
-	fread(cBuf, ulLength, 1, fFile);
-	fclose(fFile);
-	cBuf[ulLength] = 0;
-
-	return cBuf;
-
-	//std::string sShaderCode;
-	//std::ifstream ifShaderStream(cFile, std::ios::in);
-
-	//if (ifShaderStream.is_open())
-	//{
-	//	std::stringstream sstr;
-	//	sstr << ifShaderStream.rdbuf();
-	//	sShaderCode = sstr.str();
-	//	ifShaderStream.close();
-	//}
-}
-
-GLuint MakeVertexShader()
-{
-	GLchar* cVertexShaderSource = FileToBuf("Vertex_Shader_1.glsl");
-	GLuint VertexShader = mMaster.VertexShader();
-	VertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(VertexShader, 1, &cVertexShaderSource, NULL);
-	glCompileShader(VertexShader);
-
-	GLint iResult;
-	GLchar cErrorLog[512];
-	glGetShaderiv(VertexShader, GL_COMPILE_STATUS, &iResult);
-
-	if (!iResult)
-	{
-		glGetShaderInfoLog(VertexShader, 512, NULL, cErrorLog);
-		std::cerr << "ERROR: Vertex Shader Compile Failed\n" << cErrorLog << std::endl;
-
-		return FALSE;
-	}
-
-	return VertexShader;
-}
-
-GLuint MakeFragmentShader()
-{
-	GLchar* cFragmentShaderSource = FileToBuf("Fragment_Shader_1.glsl");
-	GLuint FragmentShader = mMaster.FragmentShader();
-	FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(FragmentShader, 1, &cFragmentShaderSource, NULL);
-	glCompileShader(FragmentShader);
-
-	GLint iResult;
-	GLchar cErrorLog[512];
-	glGetShaderiv(FragmentShader, GL_COMPILE_STATUS, &iResult);
-
-	if (!iResult)
-	{
-		glGetShaderInfoLog(FragmentShader, 512, NULL, cErrorLog);
-		std::cerr << "ERROR: Fragment Shader Compile Failed\n" << cErrorLog << std::endl;
-
-		return FALSE;
-	}
-
-	return FragmentShader;
-}
-
-GLuint MakeShaderProgram()
-{
-	GLuint VertexShader = MakeVertexShader();;
-	GLuint FragmentShader = MakeFragmentShader();;
-	GLuint ShaderProgramID = mMaster.ShaderProgram();
-
-	ShaderProgramID = glCreateProgram();
-	glAttachShader(ShaderProgramID, VertexShader);
-	glAttachShader(ShaderProgramID, FragmentShader);
-	glLinkProgram(ShaderProgramID);
-
-	glDeleteShader(VertexShader);
-	glDeleteShader(FragmentShader);
-
-	GLint iResult;
-	GLchar cErrorLog[512];
-	glGetProgramiv(ShaderProgramID, GL_LINK_STATUS, &iResult);
-
-	if (!iResult)
-	{
-		glGetProgramInfoLog(ShaderProgramID, 512, NULL, cErrorLog);
-		std::cerr << "ERROR: Shader Program Link Failed\n" << cErrorLog << std::endl;
-
-		return FALSE;
-	}
-
-	glUseProgram(ShaderProgramID);
-
-	return ShaderProgramID;
-}
-
-GLvoid InitializeShader()
-{
-	GLuint ShaderID = MakeShaderProgram();
-	mMaster.InputShaderID(ShaderID);
-}
+Object oOutside[VAO];
+GLuint uiVAO[VAO], uiVBO[VBO];
 
 GLvoid InitializeBuffer()
 {
 	static BOOL bInitialize = FALSE;
 
-	GLfloat Triangle[3][3], TriangleColor[3][3];
+	GLfloat Triangle[10][3][3], TriangleColor[10][3][3];
 
-	for (GLint i = 0; i < 3; ++i)
+	for (GLint k = 0; k < VAO; ++k)
 	{
-		for (GLint j = 0; j < 3; ++j)
+		for (GLint i = 0; i < 3; ++i)
 		{
-			Triangle[i][j] = oOutside.TrianglePos(i, j);
-			TriangleColor[i][j] = oOutside.Color(i, j);
+			for (GLint j = 0; j < 3; ++j)
+			{
+				Triangle[k][i][j] = oOutside[k].TrianglePos(i, j);
+				TriangleColor[k][i][j] = oOutside[k].Color(i, j);
+			}
 		}
 	}
 
 	if (!bInitialize)
 	{
-		glGenVertexArrays(VAO, &uiVAO);
+		glGenVertexArrays(VAO, uiVAO);
 		glGenBuffers(VBO, uiVBO);
 
 		bInitialize = TRUE;
 	}
 
-	glBindVertexArray(uiVAO);
+	for (GLint i = 0; i < VAO; ++i)
+	{
+		glBindVertexArray(uiVAO[i]);
 
-	glBindBuffer(GL_ARRAY_BUFFER, uiVBO[0]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Triangle, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, uiVBO[i * 2]);
+		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Triangle[i], GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, uiVBO[1]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), TriangleColor, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, uiVBO[i * 2 + 1]);
+		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), TriangleColor[i], GL_STATIC_DRAW);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
+	}
 }
 
 GLvoid DrawScene()													// 콜백 함수 : 그리기 콜백 함수
@@ -409,21 +239,25 @@ GLvoid DrawScene()													// 콜백 함수 : 그리기 콜백 함수
 	GLuint ShaderID = mMaster.ShaderID();
 	glUseProgram(ShaderID);
 
-	glBindVertexArray(uiVAO);
+	for (GLint i = 0; i < VAO; ++i)
+	{
+		glBindVertexArray(uiVAO[i]);
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
+
 	glutSwapBuffers();												// 화면에 출력하기
 }
 
 GLvoid Timer(GLint iValue)
 {
-	if (oOutside.StartTimer())
+	if (oOutside[iValue].StartTimer())
 	{
-		oOutside.ChangeDirection();
-		oOutside.ChangePos();
+		oOutside[iValue].ChangeDirection();
+		oOutside[iValue].ChangePos();
 
 		InitializeBuffer();
-		glutTimerFunc(1, Timer, 1);
+		glutTimerFunc(1, Timer, iValue);
 	}
 
 	glutPostRedisplay();
@@ -435,13 +269,21 @@ GLvoid Keyboard(GLubyte ubKey, GLint iX, GLint iY)
 	{
 	case 'S': case 's':
 	{
-		oOutside.ChangeTimer(TRUE);
-		Timer(1);
+		for (GLint i = 0; i < VAO; ++i)
+		{
+			oOutside[i].ChangeTimer(TRUE);
+			Timer(i);
+		}
+
 		break;
 	}
 	case 'T': case 't':
 	{
-		oOutside.ChangeTimer(FALSE);
+		for (GLint i = 0; i < VAO; ++i)
+		{
+			oOutside[i].ChangeTimer(FALSE);
+		}
+
 		break;
 	}
 	case 'Q': case 'q':
