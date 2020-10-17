feee333090632c7804762847558c9450751420e2
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2020-10-18 06:59:18 +0900

    Fragment Shader 통합

--- Shader/Fragment_Shader_1.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 vFragmentColor;
-in vec3 vOutColor;
-
-void main()
-{
-	vFragmentColor = vec4(vOutColor, 1.0);
-}
--- Shader/Fragment_Shader_6.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-out ver4 color;
-
-void main()
-{
-	color = verc(1.0, 0.0, 0.0, 1.0);
-}
