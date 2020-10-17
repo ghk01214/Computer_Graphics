3097bfc0e696602f8233f4fa2fa63e3fe23e3f8c
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2020-10-18 06:51:23 +0900

    실행 파일 통합
    실행 파일 Shader.cpp로 통합

--- Shader/Shader.cpp
+++ Shader/Shader.cpp
@@ -1,5 +1,5 @@
 //실습 7번
-#include "Shader_7.h"
+#include "Shader_6.h"
 
 GLvoid main(GLint iArgc, CHAR** cArgv)									//윈도우 출력하고 콜백함수 설정
 {
@@ -8,7 +8,7 @@ GLvoid main(GLint iArgc, CHAR** cArgv)									//윈도우 출력하고 콜백함수 설정
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);						// 디스플레이 모드 설정
 	glutInitWindowPosition(WINDOW_POS_X, WINDOW_POS_Y);					// 윈도우의 위치 지정
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);					// 윈도우의 크기 지정
-	glutCreateWindow("Shader 7");										// 윈도우 생성 윈도우 이름
+	glutCreateWindow("Shader");										// 윈도우 생성 윈도우 이름
 
 	//GLEW 초기화하기
 	glewExperimental = GL_TRUE;
