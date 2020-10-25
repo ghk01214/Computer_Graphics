0aa98d220a0e850dd47bbf0f40be40d8c279e689
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2020-10-26 04:33:03 +0900

    Create Vertex_Shader.glsl

---
+++ Transform/Transform/Vertex_Shader.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+
+layout(location = 0) in vec3 vPos;
+layout(location = 1) in vec3 vInColor;
+
+uniform mat4 mTransform;
+
+out vec3 vOutColor;
+
+void main()
+{
+	gl_Position = mTransform * vec4(vPos, 1.0f);
+	vOutColor = vInColor;
+}
