268fde5cced08676cf10f4cb46fdfd02d853b514
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2020-10-29 04:06:31 +0900

    Update main.h

--- Transform/Transform/main.h
+++ Transform/Transform/main.h
@@ -1,46 +1,106 @@
 #pragma once
 #include "Shader3D.h"
 
+enum TimerNum
+{
+	None, Rotate, Move
+};
+
 Shader3D sMaster;
+GLint iType = 2;
 GLint iDepth = 0;
 GLint iPolygon = 0;
-BOOL bTimer = FALSE;
-Angle Degree = {0.0f, 0.0f, 0.0f};
+GLint iRotate = 0;
+GLint iMove = 0;
+BOOL iReset = GL_FALSE;
+Pos Coord = { 0.0f, 0.0f, 0.0f };
+Angle Degree = { 0.0f, 0.0f, 0.0f };
+glm::vec3 Translate(0.0f, 0.0f, 0.0f);
 
 GLvoid Timer(GLint iValue)
 {
-	if (bTimer)
+	switch (iMove)
 	{
-		switch (iValue)
-		{
-		case 1:
-		{
-			if (Degree.fX > 360.0f)
-			{
-				Degree.fX = 0.0f;
-			}
-			else
-			{
-				Degree.fX = 1.0f;
-			}
+	case 1:
+	{
+		Translate = { 0.01f, 0.0f, 0.0f };
 
-			sMaster.ChangeRotation(FALSE);
-			sMaster.InputRotationFactor(Degree);
-			break;
-		}
-		case 2:
-		{
-
-		}
-		default:
-			break;
-		}
-
-		sMaster.InitializeBuffer();
-
-		glutTimerFunc(100, Timer, iValue);
-		glutPostRedisplay();
+		break;
 	}
+	case 2:
+	{
+		Translate = { -0.01f, 0.0f, 0.0f };
+
+		break;
+	}
+	case 3:
+	{
+		Translate = { 0.0f, 0.01f, 0.0f };
+
+		break;
+	}
+	case 4:
+	{
+		Translate = { 0.0f, -0.01f, 0.0f };
+
+		break;
+	}
+	default:
+		Translate = { 0.0f, 0.0f, 0.0f };
+
+		break;
+	}
+
+	switch (iRotate)
+	{
+	case 1:
+	{
+		Degree.fX = 1.0f;
+
+		break;
+	}
+	case 2:
+	{
+		Degree.fX = -1.0f;
+
+		break;
+	}
+	case 3:
+	{
+		Degree.fY = 1.0f;
+
+		break;
+	}
+	case 4:
+	{
+		Degree.fY = -1.0f;
+
+		break;
+	}
+	default:
+		Degree.fX = 0.0f;
+		Degree.fY = 0.0f;
+
+		break;
+	}
+
+	if (!iReset)
+	{
+		sMaster.Reset(GL_FALSE);
+		sMaster.InputTranslateCoordinate(Translate);
+		sMaster.InputRotationFactor(Degree);
+	}
+	else
+	{
+		sMaster.Reset(GL_TRUE);
+	}
+
+	sMaster.TranslateShader(GL_TRUE);
+	sMaster.RotateShader(GL_TRUE);
+
+	sMaster.InitializeBuffer();
+	glutTimerFunc(1, Timer, 1);
+	glutPostRedisplay();
 }
 
 GLvoid Keyboard(GLubyte ubKey, GLint iX, GLint iY)
@@ -49,12 +109,12 @@ GLvoid Keyboard(GLubyte ubKey, GLint iX, GLint iY)
 	{
 	case 'C': case 'c':
 	{
-
+		iType = 1;
 		break;
 	}
 	case 'P': case 'p':
 	{
-
+		iType = 2;
 		break;
 	}
 	case 'H': case 'h':
@@ -65,23 +125,33 @@ GLvoid Keyboard(GLubyte ubKey, GLint iX, GLint iY)
 	}
 	case 'x':
 	{
-		bTimer = TRUE;
-		glutTimerFunc(100, Timer, 1);
-		//Timer(1);
+		iReset = GL_FALSE;
+		iRotate = 1;
+		glutTimerFunc(1, Timer, 1);
+
 		break;
 	}
 	case 'X':
 	{
-		glutTimerFunc(1, Timer, 2);
+		iReset = GL_FALSE;
+		iRotate = 2;
+		glutTimerFunc(1, Timer, 1);
+
 		break;
 	}
 	case 'y':
 	{
+		iReset = GL_FALSE;
+		iRotate = 3;
+		glutTimerFunc(1, Timer, 1);
 
 		break;
 	}
 	case 'Y':
 	{
+		iReset = GL_FALSE;
+		iRotate = 4;
+		glutTimerFunc(1, Timer, 1);
 
 		break;
 	}
@@ -91,38 +161,17 @@ GLvoid Keyboard(GLubyte ubKey, GLint iX, GLint iY)
 
 		break;
 	}
-	case GLUT_KEY_LEFT:
-	{
-
-
-		break;
-	}
-	case GLUT_KEY_RIGHT:
-	{
-
-
-		break;
-	}
-	case GLUT_KEY_UP:
-	{
-
-
-		break;
-	}
-	case GLUT_KEY_DOWN:
-	{
-
-
-		break;
-	}
 	case 'S': case 's':
 	{
+		iRotate = 0;
+		iMove = 0;
+		iReset = GL_TRUE;
+		glutTimerFunc(1, Timer, 1);
 
 		break;
 	}
 	case 'Q': case 'q':
 	{
-
 		exit(0);
 	}
 	default:
@@ -132,9 +181,64 @@ GLvoid Keyboard(GLubyte ubKey, GLint iX, GLint iY)
 	glutPostRedisplay();
 }
 
-GLvoid KeyUp(GLubyte ubKey, GLint iX, GLint iY)
+GLvoid Special(GLint ubKey, GLint iX, GLint iY)
 {
+	switch (ubKey)
+	{
+	case GLUT_KEY_LEFT:
+	{
+		iReset = GL_FALSE;
+		iMove = 2;
+		glutTimerFunc(1, Timer, 1);
 
+		break;
+	}
+	case GLUT_KEY_RIGHT:
+	{
+		iReset = GL_FALSE;
+		iMove = 1;
+		glutTimerFunc(1, Timer, 1);
+
+		break;
+	}
+	case GLUT_KEY_UP:
+	{
+		iReset = GL_FALSE;
+		iMove = 3;
+		glutTimerFunc(1, Timer, 1);
+
+		break;
+	}
+	case GLUT_KEY_DOWN:
+	{
+		iReset = GL_FALSE;
+		iMove = 4;
+		glutTimerFunc(1, Timer, 1);
+
+		break;
+	}
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+GLvoid SpecialUp(GLint ubKey, GLint iX, GLint iY)
+{
+	switch (ubKey)
+	{
+	case GLUT_KEY_LEFT: case GLUT_KEY_RIGHT: case GLUT_KEY_UP: case GLUT_KEY_DOWN:
+	{
+		iMove = 0;
+
+		break;
+	}
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
 }
 
 GLvoid DrawScene()
@@ -160,7 +264,7 @@ GLvoid DrawScene()
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
 
-	sMaster.DrawPolygon(0.0f, 0.0f, 0.0f, 2);
+	sMaster.DrawPolygon(Coord, iType);
 
 	glutSwapBuffers();
 }
