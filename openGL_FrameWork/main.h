4447e4b2ca5fd3c8585c633a7ec5f41778a1615f
ghk01214 <ghk01214@naver.com>
2020-11-09 14:15:25 +0900

    배경 회색으로 수정

--- openGL_FrameWork/main.h
+++ openGL_FrameWork/main.h
@@ -110,7 +110,7 @@ GLvoid Passive(GLint iX, GLint iY)
 //출력 함수
 GLvoid DrawScene()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	sAdmin.Render();
